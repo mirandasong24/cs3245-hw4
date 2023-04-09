eec1b2a1cb636fb5de74ecf1fd6f092ae2c9b915
Anita Zheng <luo.b.zheng@mail.mcgill.ca>
2023-04-09 13:42:29 +0800

    added functionality to search in boolean and free text queries and added relevance feedback for free text query

--- search.py
+++ search.py
@@ -8,17 +8,20 @@ from collections import defaultdict
 import collections
 import math
 from nltk.stem.porter import *
+import shlex
+
 
 DOC_LEN_FILENAME = 'doc-len.txt'
 DOCIDS_FILENAME = 'doc-ids.txt'
-K = 10
+DOC_VECTORS_FILENAME = 'doc-vector.txt'
 
 
 class Search:
     # define common variables that are accessed in search
-    def __init__(self, dictionary, filename_postings, docIDs=None, length=None):
+    def __init__(self, dictionary, filename_postings, doc_vectors=None, docIDs=None, length=None):
         self.dictionary = dictionary
         self.FILENAME_POSTINGS = filename_postings
+        self.doc_vectors = doc_vectors
         self.docIDs = docIDs
         self.length = length
 
@@ -26,6 +29,65 @@ class Search:
 def usage():
     print("usage: " +
           sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
+    
+    
+def boolean_retrieval(query, s):
+    # returns list of docIDs that satisfy the boolean query
+    tokens = shlex.split(query) # tokenize the query, i.e. get all phrases and words
+
+    postings = []
+
+    # obtain array of postings lists to merge
+    for token in tokens:
+        if len(token.split()) > 1:
+            postings.append(phrasal_query(token)) # get docIDs for phrasal query
+        elif token != "AND":
+            token = token.lower()
+            stemmer = PorterStemmer()
+            token = stemmer.stem(token)
+
+            # fetch postings list for token
+            _, pos = s.dictionary[token]
+            with open(s.FILENAME_POSTINGS, 'rb') as f:
+                f.seek(pos)
+                postings.append(pickle.load(f))
+        else:
+            continue # if token is "AND", skip it
+    
+    # merge all postings lists using "AND" operator
+    while len(postings) > 1:
+        postings_1 = postings.pop()
+        postings_2 = postings.pop()
+        postings.append(logical_and(postings_1, postings_2))
+
+    return postings
+
+
+def phrasal_query(query, s): # takes as input the phrasal query and returns list of docIDs containing that phrase
+    pass
+
+
+def logical_and(postings_one, postings_two):
+    # returns the merged postings lists using the and operator
+
+    result = []
+    p1 = 0
+    p2 = 0
+
+    while p1 != len(postings_one) and p2 != len(postings_two):
+        docID_1 = postings_one[p1].docID
+        docID_2 = postings_two[p2].docID
+
+        if docID_1 == docID_2:
+            result.append(docID_1)
+            p1 += 1
+            p2 += 1
+        elif docID_1 < docID_2:
+            p1 += 1
+        else:
+            p2 += 1
+
+    return result
 
 
 def get_w_tq(t, count, s):
@@ -42,10 +104,9 @@ def get_w_tq(t, count, s):
     return w_tq
 
 
-def cosine_score(tokens, s):
+def cosine_score(query_vec, s):
     scores = defaultdict(float)
-    uniqueTokens = set(tokens)
-    count = collections.Counter(tokens)
+    uniqueTokens = query_vec.keys()
 
     # loop through all query terms
     for t in uniqueTokens:
@@ -53,29 +114,33 @@ def cosine_score(tokens, s):
             # if term not found, continue to next term
             continue
 
-        # calculate w(t,q)
-        w_tq = get_w_tq(t, count, s)
-
         # fetch postings list for t
         _, pos = s.dictionary[t]
         with open(s.FILENAME_POSTINGS, 'rb') as f:
             f.seek(pos)
             posting = pickle.load(f)
 
-            for doc, tf in posting:
+        # take care of zone weighting here (check if title_pos or zone_pos is empty)
+            for doc, tf, title_pos, zone_pos in posting:
                 w_td = 1 + math.log(tf, 10)     # calculate log tf for document
-                scores[doc] += (w_td * w_tq)
+                scores[doc] += (w_td * query_vec[t])
 
     for d, score in scores.items():
         scores[d] = score/s.length[d]
 
     # sort by decreasing value (score) and then increasing key (docID)
     res = sorted(scores.items(), key=lambda x: (-x[1], x[0]))
+    res2 = [] # get all docIDs with scores > 0
 
-    res = [x[0] for x in res[:K]]
+    for docID, score in res:
+        if score == 0:
+            break
+        else:
+            res2.append(docID)
+
     # for d in res:
     #     print(f'document {d} has score {scores[d]}')
-    return res
+    return res2
 
 
 def run_search(dict_file, postings_file, queries_file, results_file):
@@ -99,14 +164,40 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     with open(DOC_LEN_FILENAME, 'rb') as f:
         search.length = pickle.load(f)
 
+    # load document vectors (map of document: document vector)
+    with open(DOC_VECTORS_FILENAME, 'rb') as f:
+        search.doc_vectors = pickle.load(f)
+
+
     # output file for writing
     r = open(results_file, 'w')
 
     with open(queries_file, 'r') as f:
-        # read each query
-        for line in f:
-            # split line by whitespace
-            terms = line.split()
+        # read query
+        query = f.readline()
+        relevant_docs = f.readlines()
+        results = []
+
+        # check if query is boolean or free text
+        if "AND" in query:
+            results = boolean_retrieval(query, search)
+        else:
+            # compute centroid of relevant documents
+            centroid_vec = {}
+
+            for doc in relevant_docs:
+                for term, weight in search.doc_vectors[int(doc.strip())].items():
+                    centroid_vec.update({term: centroid_vec.get(term, 0) + weight}) 
+            
+            # divide centroid term weights by number of relevant docs
+            for term, weight in centroid_vec:
+                centroid_vec[term] = weight / len(relevant_docs)
+        
+            # compute initial query vector 
+            query_vec = {}
+
+            # split query by whitespace
+            terms = query.split()
             tokens = []
 
             # apply stemming as documents
@@ -114,18 +205,29 @@ def run_search(dict_file, postings_file, queries_file, results_file):
                 term = term.lower()
                 stemmer = PorterStemmer()                           # apply stemming
                 tokens.append(stemmer.stem(term))
-
-            results = []
+            
+            count = collections.Counter(tokens) # get term frequency in query
             # try:
-            results = cosine_score(tokens, search)
+            
+            for term in tokens:
+                query_vec[term] = get_w_tq(term, count, search)
+
+            # compute modified query based on rocchio formula
+            modified_query_vec = {}
+
+            for term in set().union(query_vec, centroid_vec): # union of terms in query and centroid vectors
+                modified_query_vec[term] = (query_vec.get(term, 0) * 0.7) + (centroid_vec.get(term, 0) * 0.3)
+
+            results = cosine_score(modified_query_vec, search)
+
             # except KeyError as err:
             #     # invalid query
             #     r.write(f'\n')
             #     continue
 
-            # format output postings list
-            resStr = ' '.join([str(x) for x in results])
-            r.write(f'{resStr}\n')
+        # format output postings list
+        resStr = ' '.join([str(x) for x in results])
+        r.write(f'{resStr}\n')
 
 
 dictionary_file = postings_file = file_of_queries = output_file_of_results = None
